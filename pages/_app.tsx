fbe04f8181b5270cedd48bcc1586f70b136eb30f
NaN <nan@thedac.info>
2023-01-04 11:56:19 -0500

    init UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import '../styles/globals.css'
+import { Work_Sans } from '@next/font/google'
+import Humane from '@next/font/local'
 import type { AppProps } from 'next/app'
 
+const workSans = Work_Sans({ subsets: ['latin'] })
+const humane = Humane({ src: '../styles/fonts/Humane-Medium.woff2' })
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --work-font: ${workSans.style.fontFamily};
+            --humane-font: ${humane.style.fontFamily};
+          }
+        `}
+      </style>
+      <Component {...pageProps} />
+    </>
+  )
 }
