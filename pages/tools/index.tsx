481c0a066472e1e1d1fac6fe8dacd04ac0795004
nan <nan@thedac.info>
2023-05-22 14:55:58 +0200

    TOOLS INDEX PAGE Updated
    - Replacing text
    _ Adding links to buttons

--- pages/tools/index.tsx
+++ pages/tools/index.tsx
@@ -15,7 +15,7 @@ export default function Tools() {
         <div className="px-10 lg:px-48">
           <h1 className="font-poppins text-5xl lg:text-9xl text-gray-normal">THE HUB<br />PLATFORM</h1>
           <div className="w-6 h-[2px] bg-gray-normal mb-7 mt-7 lg:mt-14"></div>
-          <p className="sm:w-[530px] font-light text-2xl">METAGAMEHUB brings adoption to the Metaverse through an infrastructrue platform focusing on three main pillars: Investing, Building and Experiencing</p>
+          <p className="sm:w-[530px] font-light text-2xl">The all-in-one suite of tools for investing, building, and experiencing the Metaverse</p>
         </div>
         <div className="relative w-full py-28">
           <div className="w-full overflow-hidden">
@@ -28,21 +28,21 @@ export default function Tools() {
           <Image src={'/tools/game-machine.png'} alt={'game machine'} width={340} height={309} className='md:hidden absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-[52%]' />
         </div>
         <div className="flex flex-wrap justify-center gap-x-20 gap-y-10 md:gap-y-20 py-28 md:px-20">
-          <Button url="/" big>
+          <Button url="https://app.metagamehub.io/" newTab big>
             <p className="font-humane text-[135px] text-gray-normal">INVEST</p>
           </Button>
-          <Button url="/" big>
+          <Button url="https://dcl-edit.com/" newTab big>
             <p className="font-humane text-[135px] text-gray-normal">BUILD</p>
           </Button>
-          <Button url="/" big>
+          <Button url="https://linktr.ee/metagamehub.dao" newTab big>
             <p className="font-humane text-[135px] text-gray-normal">EXPERIENCE</p>
           </Button>
         </div>
         <div className="py-28">
-          <p className="font-work font-light text-[32px] text-center px-10">&quot;Unlock the potential of the metaverse for your business&quot;</p>
+          <p className="font-work font-light text-[32px] text-center px-10">&quot;Your home in the Metaverse&quot;</p>
           <div className="flex justify-center py-28 px-10">
-            <Button url="">
-              <p className="font-work text-[22px] text-gray-normal py-12 px-10 text-center">Navigate the metaverse with us</p>
+            <Button url="https://app.metagamehub.io/" newTab>
+              <p className="font-work text-gray-normal py-6 px-10 text-center uppercase">Navigate the metaverse with us</p>
             </Button>
           </div>
         </div>
