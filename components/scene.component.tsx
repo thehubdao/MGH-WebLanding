8d48639d759767a930d619b655eb61eb4ac285db
Andres <andgonzalezcas@unal.edu.co>
2023-08-14 14:08:30 -0500

    clear three fiber js file

--- components/scene.component.tsx
+++ components/scene.component.tsx
@@ -1,35 +1,75 @@
-import { Canvas, useFrame, useThree, extend } from "@react-three/fiber";
-import { useLoader } from '@react-three/fiber'
 import { Suspense, useEffect, useMemo, useRef, useState } from "react";
+import { Html, useProgress, OrbitControls } from "@react-three/drei"
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import * as THREE from 'three'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { EffectComposer, Bloom, Vignette, Noise } from '@react-three/postprocessing'
-import { Html, OrbitControls, useProgress } from "@react-three/drei"
-import React from "react";
-import { fragmentShader, vertexShader } from "../shaders/portal.shader";
 import { gsap } from "gsap";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { fragmentShader, vertexShader } from "../shaders/portal.shader";
 
-extend({ OrbitControls });
-
-function LightBulb(props: any) {
-  return (
-    <mesh {...props} >
-      <pointLight castShadow intensity={props.intensity} color={props.color} />
-      {/* <sphereBufferGeometry args={[0.2, 30, 10]} />
-      <meshPhongMaterial emissive={"yellow"} /> */}
-    </mesh>
-  );
+function Loader() {
+  const { active, progress, errors, item, loaded, total } = useProgress();
+  return <Html fullscreen>
+    <p className="text-3xl bg-green-500 text-center pt-6">{progress} % loaded</p>
+  </Html>;
 }
 
-function LightFocus(props: any) {
+function PortalTitleAnimation() {
+  const imagePaths = [
+    '/animation/enter_the_hub/enter_the_hub_00000.png',
+    '/animation/enter_the_hub/enter_the_hub_00001.png',
+    '/animation/enter_the_hub/enter_the_hub_00002.png',
+    '/animation/enter_the_hub/enter_the_hub_00003.png',
+    '/animation/enter_the_hub/enter_the_hub_00004.png',
+    '/animation/enter_the_hub/enter_the_hub_00005.png',
+    '/animation/enter_the_hub/enter_the_hub_00006.png',
+    '/animation/enter_the_hub/enter_the_hub_00007.png',
+    '/animation/enter_the_hub/enter_the_hub_00008.png',
+    '/animation/enter_the_hub/enter_the_hub_00009.png',
+    '/animation/enter_the_hub/enter_the_hub_00010.png',
+    '/animation/enter_the_hub/enter_the_hub_00011.png',
+    '/animation/enter_the_hub/enter_the_hub_00012.png',
+    '/animation/enter_the_hub/enter_the_hub_00013.png',
+    '/animation/enter_the_hub/enter_the_hub_00014.png',
+    '/animation/enter_the_hub/enter_the_hub_00015.png',
+    '/animation/enter_the_hub/enter_the_hub_00016.png',
+    '/animation/enter_the_hub/enter_the_hub_00017.png',
+    '/animation/enter_the_hub/enter_the_hub_00018.png',
+    '/animation/enter_the_hub/enter_the_hub_00019.png',
+    '/animation/enter_the_hub/enter_the_hub_00020.png',
+    '/animation/enter_the_hub/enter_the_hub_00021.png',
+    '/animation/enter_the_hub/enter_the_hub_00022.png',
+    '/animation/enter_the_hub/enter_the_hub_00023.png',
+    '/animation/enter_the_hub/enter_the_hub_00024.png',
+    '/animation/enter_the_hub/enter_the_hub_00025.png',
+    '/animation/enter_the_hub/enter_the_hub_00026.png',
+    '/animation/enter_the_hub/enter_the_hub_00027.png',
+  ];
+
+  const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
+  const textures = useLoader(THREE.TextureLoader, imagePaths);
+  const FPS = 17;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex: number) => {
+        if (prevIndex == imagePaths.length - 1) {
+          clearInterval(interval);
+          return prevIndex;
+        }
+        return (prevIndex + 1) % imagePaths.length;
+      });
+    }, 1000 / FPS);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <mesh {...props} >
-      <spotLight castShadow intensity={props.intensity} color={props.color} angle={Math.PI / 2} />
-      {/* <sphereBufferGeometry args={[0.2, 30, 10]} />
-      <meshPhongMaterial emissive={"yellow"} /> */}
+    <mesh position={[0, 0, -5]} color='transparet'>
+      <planeGeometry args={[2.5, 2.5]} />
+      <meshBasicMaterial map={textures[currentImageIndex]} transparent />
     </mesh>
-  );
-}
+  )
+};
 
 function ImportPortal() {
   const { nodes } = useLoader(GLTFLoader, '/3d/portal.glb')
@@ -74,136 +114,24 @@ function ImportPortal() {
   )
 }
 
-/* function ImportScene() {
-  const gltf = useLoader(GLTFLoader, '/3d/portal.glb')
-
-  return (
-    <primitive object={gltf.scene} toneMapped={false} />
-  )
-} */
-
-/* function Rig() {
-  const { camera } = useThree()
-  return useFrame(({ mouse }) => {
-    camera.lookAt(camera.rotation.x, 0, 0);
-  })
-} */
-
-/* function ZoomIn() {
-  const vec = new THREE.Vector3(0, 0, 4)
-  return useFrame(({ camera }) => camera.position.lerp(vec, 0.1))
-} */
-
-function Loader() {
-  const { active, progress, errors, item, loaded, total } = useProgress();
-  return <Html fullscreen>
-    <p className="text-3xl bg-green-500 text-center pt-6">{progress} % loaded</p>
-  </Html>;
-}
-
-const ImagePlane = ({ texture }: { texture: THREE.Texture }) => {
-  const planeRef = useRef();
-
-  return (
-    <mesh ref={planeRef} position={[0, 0, -5]} color='transparet'>
-      <planeGeometry args={[2, 2]} />
-      <meshBasicMaterial map={texture} transparent />
-    </mesh>
-  );
-};
-
-function Plane() {
-  const imagePaths = [
-    '/animation/enter_the_hub/enter_the_hub_00000.png',
-    '/animation/enter_the_hub/enter_the_hub_00001.png',
-    '/animation/enter_the_hub/enter_the_hub_00002.png',
-    '/animation/enter_the_hub/enter_the_hub_00003.png',
-    '/animation/enter_the_hub/enter_the_hub_00004.png',
-    '/animation/enter_the_hub/enter_the_hub_00005.png',
-    '/animation/enter_the_hub/enter_the_hub_00006.png',
-    '/animation/enter_the_hub/enter_the_hub_00007.png',
-    '/animation/enter_the_hub/enter_the_hub_00008.png',
-    '/animation/enter_the_hub/enter_the_hub_00009.png',
-    '/animation/enter_the_hub/enter_the_hub_00010.png',
-    '/animation/enter_the_hub/enter_the_hub_00011.png',
-    '/animation/enter_the_hub/enter_the_hub_00012.png',
-    '/animation/enter_the_hub/enter_the_hub_00013.png',
-    '/animation/enter_the_hub/enter_the_hub_00014.png',
-    '/animation/enter_the_hub/enter_the_hub_00015.png',
-    '/animation/enter_the_hub/enter_the_hub_00016.png',
-    '/animation/enter_the_hub/enter_the_hub_00017.png',
-    '/animation/enter_the_hub/enter_the_hub_00018.png',
-    '/animation/enter_the_hub/enter_the_hub_00019.png',
-    '/animation/enter_the_hub/enter_the_hub_00020.png',
-    '/animation/enter_the_hub/enter_the_hub_00021.png',
-    '/animation/enter_the_hub/enter_the_hub_00022.png',
-    '/animation/enter_the_hub/enter_the_hub_00023.png',
-    '/animation/enter_the_hub/enter_the_hub_00024.png',
-    '/animation/enter_the_hub/enter_the_hub_00025.png',
-    '/animation/enter_the_hub/enter_the_hub_00026.png',
-    '/animation/enter_the_hub/enter_the_hub_00027.png',
-  ];
-
-  const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
-  const textureArray: THREE.Texture[] = [];
-  const FPS = 12;
-
-  imagePaths.map((image) => {
-    const texture = useLoader(THREE.TextureLoader, image);
-    textureArray.push(texture);
-  })
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentImageIndex((prevIndex) => {
-        if (prevIndex == imagePaths.length - 1) {
-          clearInterval(interval);
-          return prevIndex;
-        }
-        return (prevIndex + 1) % imagePaths.length;
-      });
-    }, 1000 / FPS);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  return (<ImagePlane texture={textureArray[currentImageIndex]} />)
-};
-
 export default function Scene() {
-
   return (
     <div className="w-full h-full">
       <Canvas
         shadows={true}
         className='w-full h-full'
-        camera={{ position: [0, 0, 0], fov: 25 }}
+        camera={{ position: [0, 0, .1], fov: 25 }}
         gl={{ antialias: true, toneMapping: THREE.NoToneMapping }}
         linear
         color='white'
       >
         <Suspense fallback={<Loader />}>
+          <OrbitControls maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} enableZoom={false} />
+          <pointLight position={[15, 15, 15]} intensity={4} />
           <ImportPortal />
-          {/* <OrbitControls ref={controlsRef} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} enableZoom={false} /> */}
-          <ambientLight color={"white"} intensity={10} />
-          {/* <LightBulb position={[-1, -1, 0.6]} intensity={1} color={'red'} /> */}
-          {/* <LightBulb position={[0, 3, -3]} intensity={12} color={'white'} /> */}
-          {/* <LightFocus position={[0, 6, -4]} intensity={20} color={'white'} /> */}
-          <mesh position={[-0.9, -0.62, 0]}>
-            {/* <boxGeometry attach="geometry" args={[1, 0.2, 1]} /> */}
-            <meshStandardMaterial attach="material" color={"#5f6b63"} />
-          </mesh>
-          {/* <Rig /> */}
-          {/* <ZoomIn /> */}
-          {/* <EffectComposer>
-            <Bloom intensity={1} radius={0.92} luminanceThreshold={0.7} luminanceSmoothing={0.3} height={300} mipmapBlur />
-            <Vignette eskil={false} offset={0.1} darkness={1.1} />
-            <Noise opacity={0.1} />
-          </EffectComposer> */}
-          <Plane />
-          <OrbitControls />
+          <PortalTitleAnimation />
         </Suspense>
       </Canvas>
     </div>
-  )
+  );
 }
