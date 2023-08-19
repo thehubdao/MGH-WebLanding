421b1ebd0c372c235e0c7edb6e5216414b69eca2
Andres <andgonzalezcas@unal.edu.co>
2023-08-19 08:23:51 -0500

    added howler js with a free audio

--- 3D/landing/index.experience.tsx
+++ 3D/landing/index.experience.tsx
@@ -3,16 +3,23 @@ import Postprocessing from './postprocessing.experience';
 import Lights from '../common/lights';
 import PortalScene from './portal/portalScene.experience';
 import { Howl } from 'howler';
+import { useEffect } from 'react';
 
 export default function Experience() {
-  // let sound = new Howl({
-  //   src: ['/audio/space-sound-mid-109575.mp3'],
-  //   autoplay: true,
-  //   loop: true,
-  //   volume: 0.5,
-  // });
+  const handleHowler = () => {
+    let sound = new Howl({
+      src: ['/audio/space-sound-mid-109575.mp3'],
+      autoplay: true,
+      loop: true,
+      volume: 0.5,
+    });
 
-  // sound.play();
+    sound.play();
+  }
+
+  useEffect(() => {
+    handleHowler();
+  }, [])
 
   return <>
     <OrbitControls maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} enableZoom={false} />
