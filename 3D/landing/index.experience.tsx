6f7d8b791d053eec902902b668786aed2f2740fa
Andres <andgonzalezcas@unal.edu.co>
2023-08-22 12:55:08 -0500

    Added platform scene to index experience

--- 3D/landing/index.experience.tsx
+++ 3D/landing/index.experience.tsx
@@ -4,6 +4,7 @@ import Lights from '../common/lights';
 import PortalScene from './portal/portalScene.experience';
 import { Howl } from 'howler';
 import { useEffect } from 'react';
+import PlatformScene from './platform/platformScene.experience';
 
 export default function Experience() {
   const handleHowler = () => {
@@ -11,21 +12,23 @@ export default function Experience() {
       src: ['/audio/space-sound-mid-109575.mp3'],
       autoplay: true,
       loop: true,
-      volume: 0.5,
+      volume: 0.9,
     });
 
+    sound.orientation(0, 0, 10);
     sound.play();
   }
 
   useEffect(() => {
     handleHowler();
-  }, [])
+  }, []);
 
   return <>
     <OrbitControls maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} enableZoom={false} />
 
     <Lights />
     <PortalScene />
+    <PlatformScene />
 
     <Postprocessing />
   </>
