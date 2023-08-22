233704395e18b343c1367b01c49bf3812def0636
nan <nan@thedac.info>
2023-08-22 12:52:11 -0500

    Howler spatial behavior added to portal

--- 3D/landing/index.experience.tsx
+++ 3D/landing/index.experience.tsx
@@ -3,23 +3,30 @@ import Postprocessing from './postprocessing.experience';
 import Lights from '../common/lights';
 import PortalScene from './portal/portalScene.experience';
 import { Howl } from 'howler';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
 
 export default function Experience() {
+  const ambientSound = useRef<Howl>();
   const handleHowler = () => {
-    let sound = new Howl({
-      src: ['/audio/space-sound-mid-109575.mp3'],
+    ambientSound.current = new Howl({
+      src: ['/audio/test.mp3'],
       autoplay: true,
       loop: true,
-      volume: 0.5,
+      volume: 1,
     });
-
-    sound.play();
+    Howler.pos(0, 0, 0);
+    ambientSound.current.pos(0, 0, 10);
+    ambientSound.current.play();
   }
 
   useEffect(() => {
     handleHowler();
   }, [])
+
+  useFrame(({camera}) => {
+    Howler.orientation(camera.rotation.y, 0, 0);
+  })
 
   return <>
     <OrbitControls maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} enableZoom={false} />
