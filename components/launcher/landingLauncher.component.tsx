c5b4d92c2e6abf5f9a2330f8103f62e713675039
Andres <andgonzalezcas@unal.edu.co>
2023-08-24 11:16:39 -0500

    remove use ref from landing launcher

--- components/launcher/landingLauncher.component.tsx
+++ components/launcher/landingLauncher.component.tsx
@@ -1,11 +1,10 @@
 import * as THREE from 'three';
-import { Suspense } from 'react';
+import { Suspense, useRef } from 'react';
 import { Canvas } from "@react-three/fiber";
 import Experience from '../../3D/landing/index.experience';
 import Loader from '../../3D/common/loader.experience';
 
 export default function LandingLauncher() {
-
   return (
     <div className="w-full h-full">
       <Canvas
