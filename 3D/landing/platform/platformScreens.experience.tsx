f6ebbe7c3872d54d232ab00678fda43b9cee03b9
Andres <andgonzalezcas@unal.edu.co>
2023-08-31 18:38:26 -0500

    commited platform ui

--- 3D/landing/platform/platformScreens.experience.tsx
+++ 3D/landing/platform/platformScreens.experience.tsx
@@ -5,7 +5,7 @@ import { useControls } from "leva";
 
 export default function PlatformScreens() {
   const { nodes } = useGLTF('/3d/landing/platform/SCREENS.glb',);
-  const texture = useLoader(THREE.TextureLoader, '/3D/landing/texture/UI_Texture.png');
+  // const texture = useLoader(THREE.TextureLoader, '/3D/landing/texture/UI_Texture.png');
 
   const { ScreenColor } = useControls('Platform Screens', { ScreenColor: '#666666' });
 
@@ -17,9 +17,9 @@ export default function PlatformScreens() {
       <mesh {...nodes.PLATFORM_STRUCTURE}>
 
       </mesh>
-      <mesh {...nodes.PLATFORM_UI}>
+      {/* <mesh {...nodes.PLATFORM_UI}>
         <meshBasicMaterial map={texture} transparent />
-      </mesh>
+      </mesh> */}
     </group>
   )
 }
